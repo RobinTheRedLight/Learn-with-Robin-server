9f6872e2c17b12436a97e1af391c0698f9a8cd25
RobinTheRedLight <robinmitra789@gmail.com>
2022-11-15 19:31:01 +0600

    Filtered data added

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 5000
 
+app.use(cors())
+
+const allData = require('./data/categories.json');
+const courses = require('./data/courses.json');
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send(allData)
 })
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find(c => id === c._id)
+    res.send(selectedCourse)
+});
+app.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourses = courses.filter(n => id === n.category_id)
+    res.send(selectedCourses)
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
