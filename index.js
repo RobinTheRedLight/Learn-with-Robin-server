326f6da16653e6ea885e902340c00acedd507bbc
RobinTheRedLight <robinmitra789@gmail.com>
2022-11-30 19:27:34 +0600

    Comment added

--- index.js
+++ index.js
@@ -11,19 +11,23 @@ const courses = require('./data/courses.json');
 app.get('/', (req, res) => {
     res.send(allData)
 })
+// For all courses
 app.get('/courses', (req, res) => {
     res.send(courses)
 })
+// For course by id
 app.get('/courses/:id', (req, res) => {
     const id = req.params.id;
     const selectedCourse = courses.find(c => id === c._id)
     res.send(selectedCourse)
 });
+// For courses by checkout_id
 app.get('/checkout/:id', (req, res) => {
     const id = req.params.id;
     const selectedCourse = courses.find(c => id === c.checkout_id)
     res.send(selectedCourse)
 });
+// For courses by category
 app.get('/:id', (req, res) => {
     const id = req.params.id;
     const selectedCourses = courses.filter(n => id === n.category_id)
